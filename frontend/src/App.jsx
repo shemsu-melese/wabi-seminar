93630a5db4eaf3edd994f49c4669d6e1328573fc
Shemsu Melese <shemsumelese6@gmail.com>
2026-08-11 01:52:12 +1100

    wabiseminar frontend intial vite created and custamized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='head'>Welcome to wabiseminar</h1>
+      <p className='para'>The modern meeting platform</p>
+    </div>
+  )
+}
+
+export default App
+
+
