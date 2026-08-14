f6e7ea355d22860305c2603a0474461bbe684834
HamilePower <dessalegnabdisa304@gmail.com>
2026-08-14 14:33:08 +0300

    Style Wabi Seminar home page with Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 function App() {
   return (
     <div className='container'>
-      <h1 className='head'>Welcome to wabiseminar</h1>
-      <p className='para'>The modern meeting platform</p>
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to Wabi Seminar
+        </h1>
+      <p className="text-lg text-gray-600">The modern meeting platform</p>
     </div>
   )
 }
